d039c85537a2673d2d0f3c735fb267a4f0dcccc7
gimseonjin616 <66009926+gimseonjin@users.noreply.github.com>
2022-08-05 17:56:47 +0900

    Build : Setting

---
+++ src/main/java/com/v1/dgtimes/DgtimesApplication.java
@@ -0,0 +1,13 @@
+package com.v1.dgtimes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DgtimesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DgtimesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/v1/dgtimes/DgtimesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.v1.dgtimes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DgtimesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
