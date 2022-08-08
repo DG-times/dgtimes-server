901920ed43a54d5a285ca60b017c441f81fcc5c0
gimseonjin616 <66009926+gimseonjin@users.noreply.github.com>
2022-08-08 09:00:27 +0900

    feat : Add Remark In Page Controller

--- src/main/java/com/v1/dgtimes/layer/controller/PageController.java
+++ src/main/java/com/v1/dgtimes/layer/controller/PageController.java
@@ -3,7 +3,15 @@ package com.v1.dgtimes.layer.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
+/*
+설명 : 페이지 렌더링 컨트롤로 클래스를 구현햇습니다.
 
+작성일 : 2022.08.07
+
+마지막 수정한 사람 : 김선진
+
+Todo -
+*/
 @Controller
 public class PageController {
     @GetMapping("/")
