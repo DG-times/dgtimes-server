4366406251f5b0d2f78548d9fd8854abbcdad709
ikohong88 <ikohong88@gmail.com>
2022-08-09 10:35:34 +0900

    fix : BlackKeywordRepository 인터페이스명 수정

--- src/main/java/com/v1/dgtimes/layer/repository/BlackKeywordRepository.java
+++ src/main/java/com/v1/dgtimes/layer/repository/BlackKeywordRepository.java
@@ -2,7 +2,7 @@ package com.v1.dgtimes.layer.repository;
 
 /*
 설명 : BlackKeywordRespository 인터페이스 구현
-    >
+    > 2022.08.08 인터페이스명 변경 BlackKeywordRespository -> BlackKeywordRepository
 
 작성일 : 2022.08.08
 
@@ -14,6 +14,6 @@ Todo -
 import com.v1.dgtimes.layer.model.Black_keyword;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BlackKeywordRespository extends JpaRepository<Black_keyword, Long> {
+public interface BlackKeywordRepository extends JpaRepository<Black_keyword, Long> {
     Long countByBlackKeyword(String black_keyword);
 }
