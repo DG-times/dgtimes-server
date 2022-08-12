7cbc0928b529696779dada380f02a758cda6b42e
Kong Sang Uk <qkqhfp19@naver.com>
2022-08-12 18:45:20 +0900

    fix : BookmarkServiceTest_단위테스트 생성

---
+++ src/test/java/com/v1/dgtimes/layer/service/BookmarkServiceTest.java
@@ -0,0 +1,18 @@
+package com.v1.dgtimes.layer.service;
+
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+/*
+설명 : BookmarkServiceTest 작성햇습니다.
+
+작성일 : 2022.08.12
+
+마지막 수정한 사람 : 공상욱
+
+*/
+
+@ExtendWith(MockitoExtension.class)
+public class BookmarkServiceTest {
+}
