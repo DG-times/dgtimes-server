b28985e8e5341d7a3ce25dcb1bef06d2a819cf26
AhnSangRok <71484671+AhnSangRok@users.noreply.github.com>
2022-09-04 15:34:16 +0900

    feat : @Transactional(readOnly = true) 설정

--- src/main/java/com/v2/dgtimes/layer/news/service/NewsService.java
+++ src/main/java/com/v2/dgtimes/layer/news/service/NewsService.java
@@ -11,7 +11,17 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+/*
+설명 : NewsService
+    - @Transaction(readOnly = true) 설정
+
+작성일 : 2022.09.04
+
+마지막 수정한 사람 : 안상록
+
+*/
 @Service
 @RequiredArgsConstructor
 public class NewsService {
@@ -19,11 +29,13 @@ public class NewsService {
     private final GetNewsWithLikeService getNewsWithLikeService;
     private final GetNewsWithMatchAgainstService getNewsWithMatchAgainstService;
     private final BookmarkService bookmarkService;
-    
+
+    @Transactional(readOnly = true)
     public Page<NewsResponseDto> getSearchKeywordWithLike(NewsRequestDto newsRequestDto, Pageable pageable) {
         return getNewsWithLikeService.getSearchKeyword(newsRequestDto, pageable);
     }
 
+    @Transactional(readOnly = true)
     public Page<NewsResponseDto> getSearchKeywordWithKeyword(NewsRequestDto newsRequestDto, Pageable pageable) {
         return getNewsWithKeywordService.getSearchKeyword(newsRequestDto, pageable);
     }
--- src/main/java/com/v2/dgtimes/layer/user/repository/UserRepository.java
+++ src/main/java/com/v2/dgtimes/layer/user/repository/UserRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 /*
 설명 : User의 Repositoiry 입니다.
 
-작성일 : 2022.09.03
+작성일 : 2022.09.04
 
 마지막 수정한 사람 : 안상록
 
@@ -15,21 +15,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserRepository extends JpaRepository<User, String> {
 
 }
-//@Component
-//@RequiredArgsConstructor
-//public class UserRepository {
-//    private final UserReplicaRepository replicaRepository;
-//    private final UserSourceRepository sourceRepository;
-//
-//    public void save(User user){
-//        sourceRepository.save(user);
-//    }
-//
-//    public boolean existsById(String userId){
-//        return replicaRepository.existsById(userId);
-//    }
-//
-//    public Optional<User> findById(String userId){
-//        return replicaRepository.findById(userId);
-//    }
-//}
+
